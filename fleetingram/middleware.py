5794b627926b8f9bc285c673cf3dc96069f8522c
cristian-rincon <cristian.o.rincon.b@gmail.com>
2019-12-04 18:06:09 -0500

    first

---
+++ fleetingram/middleware.py
@@ -0,0 +1,38 @@
+"""Fleetingram middleware catalog."""
+
+#Django
+
+from django.shortcuts import redirect
+from django.urls import reverse
+
+class ProfileCompletionMiddleware:
+    """Profile completion middleware.
+
+    Ensure every user that is interacting with the platform
+    have their picture and biography.
+
+    """
+
+    def __init__(self, get_response):
+        """Middleware initialization."""
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        """ Code to be executed for each request before
+         the view (and later middleware) are called."""
+        
+        if not request.user.is_anonymous:
+            if not request.user.is_staff:
+                profile = request.user.profile
+                if not profile.picture or not profile.biography:
+                    if request.path not in [reverse('users:update'),reverse('users:logout')]:
+                        return redirect('users:update')
+
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
