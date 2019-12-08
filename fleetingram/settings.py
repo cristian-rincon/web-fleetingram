416f15244c89e76b466d3308871601afeb12f189
cristian-rincon <cristian.o.rincon.b@gmail.com>
2019-12-08 09:36:48 -0500

    pruebas producción 10

--- fleetingram/settings.py
+++ fleetingram/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '03u$15(ixz@g-j%=_oehgygx%gw82zao+4a!f5(w=spco7j66s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*',]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
