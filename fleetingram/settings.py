f60469499b1b493f748324ee2a7040047f3712e6
cristian-rincon <cristian.o.rincon.b@gmail.com>
2019-12-08 12:29:16 -0500

    test production 15

--- fleetingram/settings.py
+++ fleetingram/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'fleetingram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'fleetingramdb1',
         'USER': 'crincon',
         'PASSWORD': 'Berseker00',
