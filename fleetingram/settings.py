62e6368d1b907ae359674915a752b25470a13ef1
cristian-rincon <cristian.o.rincon.b@gmail.com>
2020-01-02 21:42:05 -0500

    .

--- fleetingram/settings.py
+++ fleetingram/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 
 import os
+# Configure Django App for Heroku.
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -170,6 +172,5 @@ LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = LOGIN_URL
 
-# Configure Django App for Heroku.
-import django_heroku
+
 django_heroku.settings(locals())
